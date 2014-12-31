99a58fe9b8ad8305aeab89d14c22096fac8f6972
kanarinka <kanarinka@gmail.com>
2014-12-31 10:39:00 -0500

    telling and writing jokes - first pass

--- www/server.py
+++ www/server.py
@@ -1,8 +1,11 @@
 import os
 import ConfigParser
 import requests
-from flask import Flask
+from bson.json_util import dumps
+from flask import Flask,render_template,request
 from pymongo import MongoClient 
+from random import randint
+
 app = Flask(__name__)
 app.debug = True
 
@@ -21,6 +24,7 @@ print uri
 db_client = MongoClient(uri)
 app.db = db_client[config.get('db','name')]
 app.db_weather_collection = app.db[config.get('db','weather_collection')]
+app.db_jokes_collection = app.db[config.get('db','jokes_collection')]
 
 @app.route('/')
 def hello_world():
@@ -32,8 +36,99 @@ def hello_world():
 def save_weather_data():
 	url = 'http://api.wunderground.com/api/' + config.get('api','wunderground_key') + '/conditions/q/MA/Plymouth.json'
 	response = requests.get(url)
+
 	app.db.weather_collection.insert(response.json())
 	return response.content
 
+# Grabs latest data from DB and tells a joke
+# TODO: save the mp3 file to our server so we can reload without going to the API
+@app.route('/tellAJoke')
+def tell_a_joke():
+	
+	# Grab latest weather data from DB
+	result= app.db.weather_collection.find().sort([["_id",-1]]).limit(1)
+	if result.count() == 0:
+		return "no data in the database"
+	else:
+		result = result.next()
+
+		# Joke Logic - TBD Better. Only working on temp at the moment. 
+		temp = float(result["current_observation"]["temp_f"])
+		jokes = app.db.jokes_collection.find({ "$or" : [{"conditions.temp_f.is_greater_than" : {"$lt":temp}}, {"conditions.temp_f.is_less_than" : {"$gt":temp}}, {"conditions.temp_f.is_equal_to" : {"$eq":temp}} ]},{"joke":1})
+
+		if jokes.count() == 0:
+			joke = "hey there is no joke here because the developer is not very funny. Hahahahahaha."	
+		else:
+			random = randint(0,jokes.count()-1)
+			print random
+			if (random == 0):
+				joke = jokes.next()["joke"]
+			else:
+				joke = jokes.skip(random).next()["joke"]
+
+		print joke
+		
+		# Send out for TTS
+		audio_url = "http://tts-api.com/tts.mp3"
+		params = {"q":joke, "return_url":1}
+		print audio_url
+		response = requests.get(audio_url, params=params)
+		audio_html = '<audio controls autoplay><source src="'+ response.content +'" type="audio/mpeg">Your browser does not support the audio element.</audio>'
+
+		# Dump raw data so we can verify it's working
+		json = dumps(result,sort_keys=True, indent=4)
+
+		# Return all that
+		return audio_html + "<h1>"+joke+"</h1> <pre style=\"white-space: pre-wrap;\">" + json + "</pre>"
+
+# Interface to input new jokes based on conditional parameters
+@app.route('/writeAJoke', methods=['GET', 'POST'])
+def write_a_joke():
+
+	if request.method == 'POST':
+		variable = request.form['variable']
+		isGreaterThan = request.form['is_greater_than']
+		variableValue = request.form['variable_value']
+		joke = request.form['joke']
+		error_message = ""
+		if variableValue == None or variableValue == "":
+			error_message = "Need to specify a value"
+		if joke == None or joke == "" or joke == "Funny Joke":
+			error_message = "Need to specify a joke if you want comedy"
+		if error_message == "":
+			data = {}
+			data['joke'] = joke
+			data['conditions'] = {}
+			data['conditions'][variable] = {}
+			data['conditions'][variable][isGreaterThan] = int(variableValue) 
+			
+			app.db.jokes_collection.insert(data)
+		return render_template('writeAJoke.html', joke=joke, variable=variable, is_greater_than=isGreaterThan,variable_value=variableValue, error_message=error_message)
+	else: 
+		return render_template('writeAJoke.html')
+
 if __name__ == '__main__':
     app.run()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
